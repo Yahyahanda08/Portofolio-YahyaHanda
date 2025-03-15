2f7472972232d586981b9988c83d05e6653dafc7
Yahyahanda08 <yahyahandarestanto@gmail.com>
2025-03-15 20:49:13 +0700

    protofolio yahya

---
+++ assets/ptj.js
@@ -0,0 +1,267 @@
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle");
+navClose = document.getElementById("nav-close");
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+document.addEventListener('DOMContentLoaded', function() {
+  const strings = [
+    "Data Scienties",
+    "Frontend Developer",
+    "Machine Learning Enthusiast"
+  ];
+  let currentStringIndex = 0;
+  let currentCharacterIndex = 0;
+  const typewriterElement = document.getElementById("typewriter-effect");
+  const cursorElement = document.getElementById("cursor");
+
+  // Fungsi untuk mengetikkan teks satu per satu
+  function typeWriter() {
+    const currentString = strings[currentStringIndex];
+    if (currentCharacterIndex < currentString.length) {
+      typewriterElement.textContent += currentString.charAt(currentCharacterIndex);
+      currentCharacterIndex++;
+      setTimeout(typeWriter, 100); // Kecepatan mengetik
+    } else {
+      // Menunggu sebentar setelah selesai, lalu hapus teks
+      setTimeout(() => {
+        currentCharacterIndex = 0;
+        typewriterElement.textContent = "";
+        currentStringIndex = (currentStringIndex + 1) % strings.length;
+        typeWriter(); // Mulai mengetikkan string berikutnya
+      }, 2000); // Waktu tunggu setelah selesai menulis
+    }
+  }
+
+  // Memulai efek typewriter
+  typeWriter();
+});
+
+
+
+
+
+
+
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*======================= ACCORD SKILLS ======================*/
+
+const skillsHeader = document.querySelectorAll('.skills__header');
+
+function toggleSkills() {
+    // Toggle between adding and removing the "skills__open" and "skills__close" class
+    if(this.parentNode.classList.contains('skills__open')){
+        this.parentNode.classList.remove('skills__open');
+        this.parentNode.classList.add('skills__close');
+    } else {
+        this.parentNode.classList.remove('skills__close');
+        this.parentNode.classList.add('skills__open');
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills);
+});
+
+
+/*============== Qualification Skills ===============*/
+
+/*const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+        tab.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})      
+*/
+
+/*======================= Services Modal ===================*/
+const modalViews = document.querySelectorAll(".services__modal"),
+  modalBtns = document.querySelectorAll(".services__button"),
+  modalCloses = document.querySelectorAll(".services__modal-close");
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+    });
+  });
+});
+
+/*======================= Portfolio Swiper ===================*/
+var swiper = new Swiper(".portfolio__container", {
+  cssMode: true,
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 80) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+
+/*==================== SHOW SCROLL up ====================*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
+
+/*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "uil-sun";
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme,
+  );
+  themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
+    iconTheme,
+  );
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener("click", () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+
+
+/*==================== ARTICLES ====================*/
+let swiperarticles = new Swiper(".articles__container", {
+  loop: true,
+  grapCursor: true,
+  spaceBetween: 40,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    568: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+
+// send massage
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault();  // Mencegah form untuk submit secara default
+
+  // Ambil nilai dari input form
+  const name = document.querySelector('#name').value;
+  const email = document.querySelector('#email').value;
+  const message = document.querySelector('#message').value;
+
+  // Membuat link mailto dengan data dari form
+  const mailtoLink = `mailto:yahyahanda08@gmail.com?subject=Message from ${encodeURIComponent(name)} (${encodeURIComponent(email)})&body=${encodeURIComponent(message)}`;
+
+  // Mengarahkan ke link mailto untuk membuka aplikasi email
+  window.location.href = mailtoLink;
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Menyembunyikan spinner setelah 3 detik
+  const loader = document.getElementById("loader");
+
+  if (loader) {
+    setTimeout(() => {
+      loader.style.display = "none"; // Menyembunyikan spinner
+    }, 1000); // 3000ms = 3 detik
+  }
+});
